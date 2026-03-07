42da2bd315a9a2ab3067f05d5ee83a8e69040bae
PAPSAKYEAMPONG <papaankama1@gmail.com>
2026-03-07 16:27:34 +0000

    Completed Task Manager REST API assignment

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,16 +1,39 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-
 // 1. Extract token from Authorization header
 // 2. Verify token
 // 3. Find user
 // 4. Attach user to req.user
 // 5. Call next()
 // 6. If invalid → return 401
-
 const authMiddleware = async (req, res, next) => {
-  //  implement here
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get user from the token
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: "Not authorized" });
+    }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: "Not authorized, no token" });
+  }
 };
 
 export default authMiddleware;
